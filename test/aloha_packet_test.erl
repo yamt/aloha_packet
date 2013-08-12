766d294894a35c07ace12c8f1b87393e508e504a
YAMAMOTO Takashi <yamt@mwd.biglobe.ne.jp>
2013-08-13 00:16:38 +0900

    some more tests

--- test/aloha_packet_test.erl
+++ test/aloha_packet_test.erl
@@ -46,6 +46,20 @@ arp_term() ->
       <<242,11,164,149,235,12>>, <<192,0,2,9>>},
      {bin,<<0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0>>}].
 
+icmp_bin() ->
+    <<0,3,71,140,161,179,242,11,164,149,235,12,8,0,69,0,0,84,220,4,0,0,255,1,91,153,192,0,2,9,192,0,2,1,8,0,97,173,21,237,0,1,159,59,12,0,0,0,0,0,175,21,52,17,0,0,0,0,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,0,0,0,0,0,0,0,0>>.
+
+icmp_term() ->
+    [{ether,<<0,3,71,140,161,179>>,<<242,11,164,149,235,12>>,ip},
+     {ip,4,5,0,84,56324,0,0,0,255,icmp,23449,<<192,0,2,9>>,<<192,0,2,1>>,<<>>},
+      {icmp,8,0,25005,
+       <<21,237,0,1,159,59,12,0,0,0,0,0,175,21,52,17,0,0,0,0,16,17,18,19,20,
+         21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,
+         43,44,45,46,47,0,0,0,0,0,0,0,0>>}].
+
+remove_pad(Packet) ->
+    lists:keydelete(bin, 1, Packet).
+
 tcp_decode_test() ->
     ?assertEqual(tcp_term(), aloha_packet:decode_packet(tcp_bin())).
 
@@ -53,14 +67,16 @@ tcp_encode_test() ->
     ?assertEqual(tcp_bin(), aloha_packet:encode_packet(tcp_term())).
 
 arp_decode_test() ->
-    io:format("~p~n", [aloha_packet:decode_packet(arp_bin())]),
     ?assertEqual(arp_term(), aloha_packet:decode_packet(arp_bin())).
 
 arp_encode_test() ->
     ?assertEqual(arp_bin(), aloha_packet:encode_packet(arp_term())).
 
+icmp_decode_test() ->
+    ?assertEqual(icmp_term(), aloha_packet:decode_packet(icmp_bin())).
+
+icmp_encode_test() ->
+    ?assertEqual(icmp_bin(), aloha_packet:encode_packet(icmp_term())).
+
 ether_pad_test() ->
     ?assertEqual(arp_bin(), aloha_packet:encode_packet(remove_pad(arp_term()))).
-
-remove_pad(Packet) ->
-    lists:keydelete(bin, 1, Packet).
